59833a2bf6c05e36c46be764aa715af1568df22e
franklin <xingnan.lin@columbia.edu>
2023-09-13 15:53:26 -0400

    #microservice-1

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/donaldferguson/Dropbox/000-Examples/e6156-microservice-1/resources/old-students.json"
+        "/Users/franklin/Desktop/Columbia /3 Semester (Fall 2023)/Cloud Computing/Assignments/venv/microservice-1/resources/old-students.json"
 
     def __init__(self):
         self.students = None
